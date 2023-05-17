45783d4dc7fb8746c98729a53f27b141b164e0dd
SSK-14 <sanjaykumar1481999@gmail.com>
2023-05-17 09:20:22 +0530

    Sanjay: Fix flipkart csv data

--- csv_chat.py
+++ csv_chat.py
@@ -10,4 +10,4 @@ load_dotenv()
 filepath = "flipkart.csv"
 llm = ChatOpenAI(model_name="gpt-3.5-turbo") #OpenAI(temperature=0)
 agent = create_csv_agent(llm=llm, path=filepath, verbose=True)
-agent.run("How many total reviews are there from India? Just give me the number")
+agent.run("How many total reviews are there from Mumbai? Just give me the number")
